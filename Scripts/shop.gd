41f4fb52a24a92c02488ef554f518058c892abbf
Pipilastrax <pipilastrax@gmail.com>
2025-07-08 04:40:35 +0400

    Minor progress to shop

--- Scripts/shop.gd
+++ Scripts/shop.gd
@@ -1,9 +1,14 @@
 extends Node2D
 
 signal next_stage_button
+#TODO
+#Dictionary of all of the different dice and trinkets and their respective scene
+var dice_directory : Dictionary = {"Glitched dice": "res://Scenes/glitched_dice.tscn"}
 
 func _process(_delta: float) -> void:
 	$Panel2/stats_label.text = "Your stats:\n You have"
 
 func _on_button_button_down() -> void:
 	next_stage_button.emit()
+#TODO
+#Populate each slot of the shop with a dice to buy
