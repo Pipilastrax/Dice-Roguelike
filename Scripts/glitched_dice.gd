4cddf26a32eca1c2e2f5be583b4dd38a99d09e00
Pipilastrax <pipilastrax@gmail.com>
2025-07-09 11:44:06 +0200

    Glitched dice now has an effect

--- Scripts/glitched_dice.gd
+++ Scripts/glitched_dice.gd
@@ -3,4 +3,15 @@ func _init():
 	dicename = "Glitched Dice"
 
 func roll():
-	pass
+	reroll = false
+	value = randi_range(mindice,maxdice)
+	#rerolls if value is 2 or 1
+	if value < 3:
+		value = randi_range(mindice,maxdice)
+
+	$AnimatedSprite2D.animation = "default"
+	$AnimatedSprite2D.play()
+	await get_tree().create_timer(0.5).timeout
+	$AnimatedSprite2D.stop()
+	$AnimatedSprite2D.frame = value - 1
+	print("the new GLITCHED DICE value is "+ str(value))
--- Scripts/shop.gd
+++ Scripts/shop.gd
@@ -1,9 +1,26 @@
 extends Node2D
 
 signal next_stage_button
-
+var all_dice_dict :Dictionary = {
+	0:{
+		"Name": "Dice",
+		"Cost": 20,
+		"Rarity": 1,
+		"Flavor": "Just a regular dice",
+		"Path":"res://Scenes/dice.tscn"},
+	1:{
+		"Name": "Glitched Dice",
+		"Cost": 30,
+		"Rarity": 2,
+		##TODO un mejor flavor para esto xd
+		"Flavor": "Its Glitched",
+		"Path":"res://Scenes/glitched_dice.tscn"}
+}
 func _process(_delta: float) -> void:
 	$Panel2/stats_label.text = "Your stats:\n You have"
 
 func _on_button_button_down() -> void:
 	next_stage_button.emit()
+
+func store_populator():
+	pass
